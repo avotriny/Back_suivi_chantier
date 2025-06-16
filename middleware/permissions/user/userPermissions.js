73069361ec39c8ecc0d4bc36c50af80fa1832938
Avotriniaina <avotriniainajonatan@gmail.com>
2025-06-16 21:08:27 +0200

    Permission pour user

---
+++ middleware/permissions/user/userPermissions.js
@@ -0,0 +1,11 @@
+const userPermissions = {
+    listUsers: {
+      roles: ['Directeur de chantier', 'Chef de controle/Maître d\'ouvrage'],
+    },
+    updateStatus: {
+      roles: ['Directeur de chantier'],
+    },
+  };
+  
+  export default userPermissions;
+  
