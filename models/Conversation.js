bcb0812030ff787ee851f2c56ad0fcb2f1197a99
Avotriniaina <avotriniainajonatan@gmail.com>
2025-06-21 08:19:21 +0200

    Modification modele de conversation

--- models/Conversation.js
+++ models/Conversation.js
@@ -2,13 +2,17 @@
 import mongoose from 'mongoose';
 
 const conversationSchema = new mongoose.Schema({
-  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  participants: [
+    { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+  ],
   lastMessage: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Message'
   },
-  updatedAt: { type: Date, default: Date.now }
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-const Conversation = mongoose.model('Conversation', conversationSchema); 
-export default Conversation;
+export default mongoose.model('Conversation', conversationSchema);
