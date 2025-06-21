4271aa0e92dd824912ad8b9cabcab112c7566cd2
Avotriniaina <avotriniainajonatan@gmail.com>
2025-06-21 08:19:49 +0200

    Modification de modele de message

--- models/Message.js
+++ models/Message.js
@@ -1,12 +1,26 @@
+// models/Message.js
 import mongoose from 'mongoose';
 
 const messageSchema = new mongoose.Schema({
-  senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  receiverId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  text: { type: String, required: true },
-  file: { type: String }, // Ajout du champ file pour le chemin du fichier
-  createdAt: { type: Date, default: Date.now }
+  senderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  receiverId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  fileUrl: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-const Message = mongoose.model('messages', messageSchema);
-export default Message;
+export default mongoose.model('Message', messageSchema);
